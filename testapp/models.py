ca219eda4f854424d32488872afb21c289c21bf5
datob12321 <dbitsadze01@gmail.com>
2024-05-07 19:52:10 +0400

    adding profile model

--- testapp/models.py
+++ testapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class User_Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(upload_to='profile_pics', default='default_profile_pic')
+    bio = models.TextField(blank=True, null=True)
+    location = models.TextField(blank=True, null=True)
+    followers = models.IntegerField(default=0)
+    following = models.IntegerField(default=0)
+
