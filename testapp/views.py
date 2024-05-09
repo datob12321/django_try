58378e4227eea19064d9fe96644622063c86e953
datob12321 <dbitsadze01@gmail.com>
2024-05-09 22:59:55 +0400

    new branch

--- testapp/views.py
+++ testapp/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login as log_in, logout
 
 
 # Create your views here.
 def index(request):
 
     return render(request, 'index.html')
+
+
+def login(request):
+    return render(request, 'login')
