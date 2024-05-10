9eb93011d1571d78a694201f83eb47646f67927b
baqari1991 <137100801+baqari1991@users.noreply.github.com>
2024-05-10 21:12:46 +0400

    create django app

--- testapp/urls.py
+++ testapp/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('signup', views.signup, name='signup'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
+    path('settings', views.settings, name='settings'),
+   \
+
 ]
--- testapp/views.py
+++ testapp/views.py
@@ -1,7 +1,55 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib.auth.models import User
+from .models import User_Profile
 
 
 # Create your views here.
 def index(request):
+    return HttpResponse('Hello World')
 
-    return render(request, 'index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Taken')
+                return redirect('signup')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Taken')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+
+                # Log user in and redirect to setting page
+
+
+                # create a profile object for new user
+                user_mode = User.objects.get(username=username)
+                new_profile = User_Profile.objects.create(user=user_mode)
+                new_profile.save()
+                return redirect('settings')
+        else:
+            messages.info(request, 'Password not matching')
+            return redirect('signup')
+
+    else:
+        return render(request, 'signup.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def logout(request):
+    return HttpResponse('Logout')
+
+
+def settings(request):
+    return HttpResponse('Settings')
