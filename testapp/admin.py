dde453196d37acb81843cd14999ae334b7e58bd5
datob12321 <dbitsadze01@gmail.com>
2024-05-16 00:33:34 +0400

    adding post card

--- testapp/admin.py
+++ testapp/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import User_Profile, Post
+from .models import User_Profile, Post, CommentPost
 
 # Register your models here.
 admin.site.register(User_Profile)
 admin.site.register(Post)
-
+admin.site.register(CommentPost)
--- testapp/models.py
+++ testapp/models.py
@@ -6,20 +6,30 @@ from django.contrib.auth.models import User
 
 class User_Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_picture = models.ImageField(upload_to='profile_pics', default='default_profile_pic')
+    profile_picture = models.ImageField(upload_to='profile_pics', default='default_profile_pic.jpg')
     bio = models.TextField(blank=True, null=True)
     location = models.TextField(blank=True, null=True)
     followers = models.IntegerField(default=0)
     following = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.user.username
+
 
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_profile = models.ForeignKey(User_Profile, on_delete=models.CASCADE)
     post_text = models.TextField(blank=True, null=True)
     post_image = models.ImageField(upload_to='post_pics', blank=True, null=True)
     post_video = models.FileField(upload_to='post_video', blank=True, null=True)
     likes = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class CommentPost(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_profile = models.ForeignKey(User_Profile, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
 
